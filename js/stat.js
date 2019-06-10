5538df76869123dc11a56fe983a53c23a946e91c
Ekaterina <45285300+EkaterinaEliseeva@users.noreply.github.com>
2019-06-10 23:47:52 +0300

    Добавлены похожие персонажи

--- js/stat.js
+++ js/stat.js
@@ -32,15 +32,6 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
-var roundToWhole = function (arr) {
-
-  for (var i = 0; i < arr.length; i++) {
-    arr[i] = Math.round(arr[i]);
-  }
-
-  return arr;
-};
-
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, 'rgb(255, 255, 255)');
@@ -49,17 +40,17 @@ window.renderStatistics = function (ctx, names, times) {
   renderText(ctx, CLOUD_X + 2 * GAP, CLOUD_Y + 3 * GAP, 'Ура вы победили!', 'rgb(0, 0, 0)');
   renderText(ctx, CLOUD_X + 2 * GAP, CLOUD_Y + FONT_SIZE + 4 * GAP, 'Список результатов:', 'rgb(0, 0, 0)');
 
-  var maxTime = getMaxElement(roundToWhole(times));
+  var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
     renderText(ctx, (CLOUD_X + 3 * GAP) + (BAR_GAP + TEXT_WIDTH) * i, CLOUD_Y + 2 * FONT_SIZE + 8 * GAP + BAR_HEIGHT, names[i], 'rgb(0, 0, 0)');
-    renderText(ctx, (CLOUD_X + 3 * GAP) + (BAR_GAP + TEXT_WIDTH) * i, CLOUD_Y + 2 * FONT_SIZE + 5 * GAP + BAR_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime, times[i], 'rgb(0, 0, 0)');
+    renderText(ctx, (CLOUD_X + 3 * GAP) + (BAR_GAP + TEXT_WIDTH) * i, CLOUD_Y + 2 * FONT_SIZE + 5 * GAP + BAR_HEIGHT - (BAR_HEIGHT * Math.floor(times[i]) / Math.floor(maxTime)), Math.floor(times[i]), 'rgb(0, 0, 0)');
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle = 'rgba(0, 77, 255,' + Math.random() + ')';
+      ctx.fillStyle = 'hsl(225,' + (Math.random() * 100) + '%, 50%)';
     }
-    ctx.fillRect((CLOUD_X + 3 * GAP) + (BAR_GAP + TEXT_WIDTH) * i, CLOUD_Y + 2 * FONT_SIZE + 6 * GAP + BAR_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime, TEXT_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
+    ctx.fillRect((CLOUD_X + 3 * GAP) + (BAR_GAP + TEXT_WIDTH) * i, CLOUD_Y + 2 * FONT_SIZE + 6 * GAP + BAR_HEIGHT - (BAR_HEIGHT * Math.floor(times[i]) / Math.floor(maxTime)), TEXT_WIDTH, BAR_HEIGHT * Math.floor(times[i]) / Math.floor(maxTime));
   }
 };
 
